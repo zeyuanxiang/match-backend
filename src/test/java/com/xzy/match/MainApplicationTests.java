43119f565ed50b2e2636683c33d85ec27e4cd19c
yysxzy <1841025828@qq.com>
2025-02-10 13:30:25 +0800

    项目初始化

--- src/test/java/com/xzy/match/MainApplicationTests.java
+++ src/test/java/com/xzy/match/MainApplicationTests.java
@@ -11,12 +11,5 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MainApplicationTests {
 
-    @Resource
-    private WxOpenConfig wxOpenConfig;
-
-    @Test
-    void contextLoads() {
-        System.out.println(wxOpenConfig);
-    }
 
 }
