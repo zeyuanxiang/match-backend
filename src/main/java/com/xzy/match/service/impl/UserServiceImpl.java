19d9b15962f343ad7925c7c5600b93c9247bfaa0
yysxzy <1841025828@qq.com>
2025-02-10 15:23:55 +0800

    根据标签搜索用户

--- src/main/java/com/xzy/match/service/impl/UserServiceImpl.java
+++ src/main/java/com/xzy/match/service/impl/UserServiceImpl.java
@@ -3,7 +3,6 @@ package com.xzy.match.service.impl;
 import static com.xzy.match.constant.UserConstant.USER_LOGIN_STATE;
 
 import cn.hutool.core.collection.CollUtil;
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.google.common.reflect.TypeToken;
@@ -25,7 +24,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import javax.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
-import me.chanjar.weixin.common.bean.WxOAuth2UserInfo;
+import net.bytebuddy.description.method.MethodDescription;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -121,37 +120,37 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
         return this.getLoginUserVO(user);
     }
 
-    @Override
-    public LoginUserVO userLoginByMpOpen(WxOAuth2UserInfo wxOAuth2UserInfo, HttpServletRequest request) {
-        String unionId = wxOAuth2UserInfo.getUnionId();
-        String mpOpenId = wxOAuth2UserInfo.getOpenid();
-        // 单机锁
-        synchronized (unionId.intern()) {
-            // 查询用户是否已存在
-            QueryWrapper<User> queryWrapper = new QueryWrapper<>();
-            queryWrapper.eq("unionId", unionId);
-            User user = this.getOne(queryWrapper);
-            // 被封号，禁止登录
-            if (user != null && UserRoleEnum.BAN.getValue().equals(user.getUserRole())) {
-                throw new BusinessException(ErrorCode.FORBIDDEN_ERROR, "该用户已被封，禁止登录");
-            }
-            // 用户不存在则创建
-            if (user == null) {
-                user = new User();
-                user.setUnionId(unionId);
-                user.setMpOpenId(mpOpenId);
-                user.setUserAvatar(wxOAuth2UserInfo.getHeadImgUrl());
-                user.setUserName(wxOAuth2UserInfo.getNickname());
-                boolean result = this.save(user);
-                if (!result) {
-                    throw new BusinessException(ErrorCode.SYSTEM_ERROR, "登录失败");
-                }
-            }
-            // 记录用户的登录态
-            request.getSession().setAttribute(USER_LOGIN_STATE, user);
-            return getLoginUserVO(user);
-        }
-    }
+//    @Override
+//    public LoginUserVO userLoginByMpOpen(WxOAuth2UserInfo wxOAuth2UserInfo, HttpServletRequest request) {
+//        String unionId = wxOAuth2UserInfo.getUnionId();
+//        String mpOpenId = wxOAuth2UserInfo.getOpenid();
+//        // 单机锁
+//        synchronized (unionId.intern()) {
+//            // 查询用户是否已存在
+//            QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+//            queryWrapper.eq("unionId", unionId);
+//            User user = this.getOne(queryWrapper);
+//            // 被封号，禁止登录
+//            if (user != null && UserRoleEnum.BAN.getValue().equals(user.getUserRole())) {
+//                throw new BusinessException(ErrorCode.FORBIDDEN_ERROR, "该用户已被封，禁止登录");
+//            }
+//            // 用户不存在则创建
+//            if (user == null) {
+//                user = new User();
+//                user.setUnionId(unionId);
+//                user.setMpOpenId(mpOpenId);
+//                user.setUserAvatar(wxOAuth2UserInfo.getHeadImgUrl());
+//                user.setUserName(wxOAuth2UserInfo.getNickname());
+//                boolean result = this.save(user);
+//                if (!result) {
+//                    throw new BusinessException(ErrorCode.SYSTEM_ERROR, "登录失败");
+//                }
+//            }
+//            // 记录用户的登录态
+//            request.getSession().setAttribute(USER_LOGIN_STATE, user);
+//            return getLoginUserVO(user);
+//        }
+//    }
 
     /**
      * 获取当前登录用户
