63ce94ebfe6ba5561d9276a6385f1494bebc90ef
yysxzy <1841025828@qq.com>
2025-02-11 14:15:46 +0800

    用户推荐

---
+++ src/main/java/com/xzy/match/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.xzy.match.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xzy.match.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
